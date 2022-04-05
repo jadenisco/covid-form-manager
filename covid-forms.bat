e3e4bf00e6368a7f1c515fd9b9861481efc2a849
John DeNisco <jadenisco17@gmail.com>
2022-04-05 19:04:45 -0400

    Get file input in the bat file

--- covid-forms.bat
+++ covid-forms.bat
@@ -1,8 +1,11 @@
 @echo off
 echo ---- Manage Covid Forms ----
 echo:
+cd forms
+set /P filename=Please enter the file to split: 
+cd ..
 set VROOT=z:\Developer\Windows\testroot
 set PROOT=scripts\covid-form-manager
 set VENV=%VROOT%\%PROOT%\.venv
-%VENV%\Scripts\activate.bat & python %VROOT%\%PROOT%\covid-forms.py -h & timeout /t 30 & deactivate
+%VENV%\Scripts\activate.bat & python %VROOT%\%PROOT%\covid-forms.py split %VROOT%\%PROOT%\forms\%filename% & timeout /t 30 & deactivate
 exit
