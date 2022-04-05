78a8f206bf640d71d18690c5a59d0da0d03a9143
John DeNisco <jadenisco@gmail.com>
2022-04-05 16:57:12 -0400

    Add covid-forms script

--- covid-forms.bat
+++ covid-forms.bat
@@ -1,10 +1,8 @@
 @echo off
 echo ---- Manage Covid Forms ----
 echo:
-set VROOT=z:\Developer\testroot\
-set PROOT=.Volunteer Files\ADULT MEDICAL AND NONMEDICAL\350 DeNisco, John\
-set VENV="%VROOT%%PROOT%" 
-echo %VROOT%
-rem cd %VROOT%
-dir /a %VENV%
+set VROOT=z:\Developer\Windows\testroot
+set PROOT=scripts\covid-form-manager
+set VENV=%VROOT%\%PROOT%\.venv
+%VENV%\Scripts\activate.bat & python %VROOT%\%PROOT%\covid-forms.py -h & timeout /t 30 & deactivate
 exit
